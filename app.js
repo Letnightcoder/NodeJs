90ea5679c6bc9c8699d4d82a352e88ede9937bf5
Official-Kallol <official.kallol2024@gmail.com>
2025-07-13 11:34:45 +0530

    Path module in NodeJS

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const path = require("path");
 
 // fs.readFile('example.txt','utf-8',(err, data) =>{
 //     if(err){
@@ -21,12 +22,19 @@ const http = require("http");
 // })
 
 
-const server = http.createServer((req, res)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html'); // text/plain
-    res.end('Hello, Node.js Server!');
-});
+// const server = http.createServer((req, res)=>{
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/html'); // text/plain
+//     res.end('Hello, Node.js Server!');
+// });
 
-server.listen(3000, () => {
-    console.log('Server running at http://localhost:3000');
-});
+// server.listen(3000, () => {
+//     console.log('Server running at http://localhost:3000');
+// });
+
+
+const directory = '/user/local'
+const fileName = 'example.txt'
+
+const fullPath = path.join(directory, fileName)
+console.log(fullPath);
